c08be9e152dd9da5aa7a9808846e1fd64a04bbce
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-06-12 06:39:30 -0500

    [Add]: imperative state

--- src/components/Home/ClassState/ClassState.tsx
+++ src/components/Home/ClassState/ClassState.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Loading } from './Loading';
+const SECURITY_CODE = 'paradigma';
 interface Props {
 	name: string;
 }
 interface State {
 	error: boolean;
 	loading: boolean;
+	value: string;
 }
 
 class ClassState extends React.Component<Props, State> {
@@ -14,6 +16,7 @@ class ClassState extends React.Component<Props, State> {
 		this.state = {
 			error: false,
 			loading: false,
+			value: '',
 		};
 	}
 	UNSAFE_componentWillMount() {
@@ -26,29 +29,42 @@ class ClassState extends React.Component<Props, State> {
 	componentDidUpdate(): void {
 		if (this.state.loading) {
 			setTimeout(() => {
+				let isValid = false;
+				if (this.state.value === SECURITY_CODE) {
+					isValid = true;
+				}
 				this.setState((prevState) => ({
 					...prevState,
 					loading: false,
-					error: true,
+					error: !isValid,
 				}));
 			}, 300);
 		}
 	}
 	render(): React.ReactNode {
+		const { error, loading, value } = this.state;
 		const handleValidate = () => {
 			this.setState({
 				...this.state,
 				loading: true,
 			});
 		};
+		const handleValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+			this.setState((state) => ({
+				...state,
+				error: false,
+				value: event.target.value,
+			}));
+		};
 
 		return (
 			<div className="containerContent">
 				<h2>Eliminar {this.props.name}</h2>
 				<p>Por favor, escriba el código de seguridad.</p>
-				{this.state.error && <p>Error: el codigo es incorrecto</p>}
-				{this.state.loading && <Loading />}
-				<input type="text" placeholder="Codigo de seguridad" />
+				<p>{value}</p>
+				{error && !loading && <p>Error: el codigo es incorrecto</p>}
+				{loading && <Loading />}
+				<input value={value} onInput={handleValue} type="text" placeholder="Codigo de seguridad" />
 				<button onClick={handleValidate}>Comprobar</button>
 			</div>
 		);
