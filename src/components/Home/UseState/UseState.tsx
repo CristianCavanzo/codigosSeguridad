f17be1064f4d3b941c2f4ca33eb3a4d44f877d45
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-06-12 07:12:19 -0500

    [Ad]

--- src/components/Home/UseState/UseState.tsx
+++ src/components/Home/UseState/UseState.tsx
@@ -1,3 +1,4 @@
+import { stat } from 'fs';
 import React, { useEffect, useState } from 'react';
 const SECURITY_CODE = 'paradigma';
 const UseState = ({ name }) => {
@@ -5,20 +6,35 @@ const UseState = ({ name }) => {
 		error: false,
 		loading: false,
 		value: '',
+		deleted: false,
+		confirmed: false,
 	});
+
+	const handleConfirm = () => {
+		setState({
+			...state,
+			loading: !state.loading,
+			error: false,
+			confirmed: true,
+			value: '',
+		});
+	};
+	const handleError = () => {
+		setState({
+			...state,
+			error: true,
+			confirmed: false,
+			loading: !state.loading,
+		});
+	};
 
 	useEffect(() => {
 		if (state.loading) {
 			setTimeout(() => {
-				let isValid = false;
 				if (state.value === SECURITY_CODE) {
-					isValid = true;
+					return handleConfirm();
 				}
-				setState({
-					...state,
-					loading: !state.loading,
-					error: !isValid,
-				});
+				handleError();
 			}, 500);
 		}
 	}, [state]);
@@ -38,23 +54,54 @@ const UseState = ({ name }) => {
 		});
 	};
 
-	return (
-		<div className="containerContent">
-			<h2>Eliminar {name}</h2>
-			<p>Por favor, escriba el código de seguridad.</p>
+	const handleDelete = () => {
+		setState({
+			...state,
+			deleted: true,
+		});
+	};
+	const handleDontDelete = () => {
+		setState({
+			...state,
+			confirmed: false,
+			deleted: false,
+		});
+	};
 
-			{state.error && !state.loading && <p>Error: el codigo es incorrecto</p>}
-			{state.loading && <p>Cargando...</p>}
-			{state.value}
-			<input
-				value={state.value}
-				type="text"
-				placeholder="Codigo de seguridad"
-				onInput={handleInput}
-			/>
-			<button onClick={handleValidate}>Comprobar</button>
-		</div>
-	);
+	if (!state.deleted && !state.confirmed) {
+		return (
+			<div className="containerContent">
+				<h2>Eliminar {name}</h2>
+				<p>Por favor, escriba el código de seguridad.</p>
+
+				{state.error && !state.loading && <p>Error: el codigo es incorrecto</p>}
+				{state.loading && <p>Cargando...</p>}
+				{state.value}
+				<input
+					value={state.value}
+					type="text"
+					placeholder="Codigo de seguridad"
+					onInput={handleInput}
+				/>
+				<button onClick={handleValidate}>Comprobar</button>
+			</div>
+		);
+	} else if (state.confirmed && !state.deleted) {
+		return (
+			<div className="containerContent">
+				<h2>Pedimos confirmacion. Seguro, segurin?</h2>
+				<button onClick={handleDelete}>Si, eliminar</button>
+				<button onClick={handleDontDelete}>No, eliminar</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className="containerContent">
+				<h2>Eliminado con exito</h2>
+				<button onClick={handleDontDelete}>Reset</button>
+			</div>
+		);
+	}
 };
 
 export { UseState };
